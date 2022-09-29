999f2821a83bc792b1be98a21734ca3e1b4004c1
Robin <robinslot@hotmail.nl>
2022-09-29 11:05:59 +0200

    apparently, function cannot be restored or CPU comes back

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 ;(function() {
     setTimeout(() => {
         console.warn('Fix Canvas CPU extension is running, you may see error messages from the repeating task crashing');
-        const oldFunction = window.postMessage;
-        window.postMessage = undefined; // 100% CPU task will now crash
-        // Restore function in case it is used by something else
-        setTimeout(() => window.postMessage = oldFunction, 2000);
+        // 100% CPU task will crash without postMessage function
+        // Missing function doesn't appear to affect functionality
+        window.postMessage = undefined;
     }, 15000);
 })();
